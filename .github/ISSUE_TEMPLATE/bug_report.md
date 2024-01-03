38f2209ead1c617a0158bf4a5511916946e5aa0d
Koki Ibukuro <koki.ibukuro@gmail.com>
2024-01-03 14:56:24 +0100

    Add issue template

---
+++ .github/ISSUE_TEMPLATE/bug_report.md
@@ -0,0 +1,32 @@
+---
+name: Bug report
+about: Create a report to help us improve
+title: ''
+labels: ''
+assignees: ''
+
+---
+
+**Environment (please complete the following information):**
+ - OS/OS Version: [e.g. iOS 11.0/Andoroid 8.1/macOS 10.15.6]
+ - Source Version: [e.g. master/v2.2.0]
+ - Unity Version: [e.g. Unity 2019.4.5.f1]
+
+**Describe the bug**
+A clear and concise description of what the bug is.
+
+**To Reproduce**
+Steps to reproduce the behavior:
+1. Go to '...'
+2. Click on '....'
+3. Scroll down to '....'
+4. See error
+
+**Expected behavior**
+A clear and concise description of what you expected to happen.
+
+**Screenshots**
+If applicable, add screenshots to help explain your problem.
+
+**Additional context**
+Add any other context about the problem and error logs here.
--- download-binaries.sh
+++ download-binaries.sh
@@ -10,7 +10,7 @@ fi
 
 # Define Variables
 PROJCET_DIR="$(cd "$(dirname "$0")" && pwd -P)"
-PLUGINS_DIR="$PROJCET_DIR/com.github.asus4.onnxruntime/Plugins"
+PLUGINS_CORE_DIR="$PROJCET_DIR/com.github.asus4.onnxruntime/Plugins"
 
 VTAG=$1
 TAG=$(echo $VTAG | sed 's/v//g') # Remove the leading v
@@ -27,44 +27,27 @@ function download_github_releases() {
     BASE_URL=https://github.com/microsoft/onnxruntime/releases/download/$VTAG
     echo "Downloading from $BASE_URL/$FILE_NAME"
     curl -L $BASE_URL/$FILE_NAME -o $FILE_NAME
-}
 
-function extract_releases_zip() {
-    SRC_DIR=$1
-    TARGET_FILES=$2
-    DST_DIR=$3
-    unzip -o $SRC_DIR.zip
-    cp $SRC_DIR/$TARGET_FILES $DST_DIR
-    
-    # Keep them for debugging
-    # rm -rf $FILE_NAME
-    # rm -rf $FILE_NAME.zip
-}
-
-function extract_releases_tgz() {
-    SRC_DIR=$1
-    TARGET_FILES=$2
-    DST_DIR=$3
-    tar -xzf $SRC_DIR.tgz
-    # Copy & Resolve symbolic links
-    cp -RL $SRC_DIR/$TARGET_FILES $DST_DIR
-    
-    # Keep them for debugging
-    # rm -rf $SRC_DIR
-    # rm -rf $SRC_DIR.tgz
+    # If .zip
+    if [[ $FILE_NAME =~ \.zip$ ]]; then
+        unzip -o $FILE_NAME
+    # if .tgz
+    elif [[ $FILE_NAME =~ \.tgz$ ]]; then
+        tar -xzf $FILE_NAME
+    fi
 }
 
 # Windows x64
 download_github_releases onnxruntime-win-x64-gpu-$TAG.zip
-extract_releases_zip onnxruntime-win-x64-gpu-$TAG "lib/*.dll" $PLUGINS_DIR/Windows/x64
+cp "onnxruntime-win-x64-gpu-$TAG/lib/*.dll" $PLUGINS_CORE_DIR/Windows/x64
 
 # macOS Universal
 download_github_releases onnxruntime-osx-universal2-$TAG.tgz
-extract_releases_tgz onnxruntime-osx-universal2-$TAG "lib/libonnxruntime.$TAG.dylib" $PLUGINS_DIR/macOS/libonnxruntime.dylib
+cp "onnxruntime-osx-universal2-$TAG/lib/libonnxruntime.$TAG.dylib" $PLUGINS_CORE_DIR/macOS/libonnxruntime.dylib
 
 # Linux x64
 download_github_releases onnxruntime-linux-x64-gpu-$TAG.tgz
-extract_releases_tgz onnxruntime-linux-x64-gpu-$TAG "lib/*.so" $PLUGINS_DIR/Linux/x64/
+cp -RL "onnxruntime-linux-x64-gpu-$TAG/lib/*.so" $PLUGINS_CORE_DIR/Linux/x64/
 
 echo "Done."
 exit 0

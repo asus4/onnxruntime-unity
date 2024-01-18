e173a23c9461663345578db48e1a6db9afda5de5
Koki Ibukuro <koki.ibukuro@gmail.com>
2024-01-18 16:03:01 +0100

    Add todo

--- scripts/download-onnx-extensions-libs.sh
+++ scripts/download-onnx-extensions-libs.sh
@@ -55,6 +55,7 @@ function download_package() {
 }
 
 function download_github_releases() {
+    # TODO: consider location of build libraries
     download_package $1 https://github.com/asus4/onnxruntime-unity/releases/download/v0.1.8/
 }
 

4f58f755721616ad7ceb5992b4e8b7a85946aaf7
Koki Ibukuro <koki.ibukuro@gmail.com>
2025-04-03 12:44:32 +0200

    Rename macOS library name

--- com.github.asus4.onnxruntime-genai/Runtime/NativeMethods.cs
+++ com.github.asus4.onnxruntime-genai/Runtime/NativeMethods.cs
@@ -1,3 +1,15 @@
+// Modified for Unity
+#if UNITY_ANDROID && !UNITY_EDITOR
+#define __ANDROID__
+#define __MOBILE__
+#elif UNITY_IOS && !UNITY_EDITOR
+#define __IOS__
+#define __ENABLE_COREML__
+#define __MOBILE__
+#elif UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+#define __ENABLE_COREML__
+#endif
+
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 

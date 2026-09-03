2422ca30679737adc7cbc8c99ff0cb1ebd0fad8f
Koki Ibukuro <asus4@users.noreply.github.com>
2026-09-03 22:30:46 +0200

    Fix typo

--- scripts/release-npm.sh
+++ scripts/release-npm.sh
@@ -4,7 +4,7 @@
 npm publish packages/com.github.asus4.onnxruntime/*.tgz --tag latest
 npm publish packages/com.github.asus4.onnxruntime.unity/*.tgz --tag latest
 npm publish packages/com.github.asus4.onnxruntime-extensions/*.tgz --tag latest
-npm publish packages/com.github.asus4.onnxruntime-genai/*.tgz --tag latests
+npm publish packages/com.github.asus4.onnxruntime-genai/*.tgz --tag latest
 
 # Disabled due to file size limit in npm
 # npm publish packages/com.github.asus4.onnxruntime.linux-x64-gpu/*.tgz --tag latest

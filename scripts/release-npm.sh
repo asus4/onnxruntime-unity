347eb5abb089619464e91c892fff315763819975
Koki Ibukuro <asus4@users.noreply.github.com>
2026-09-04 13:05:42 +0200

    Upload the win and linux GPU packages again

--- scripts/release-npm.sh
+++ scripts/release-npm.sh
@@ -6,9 +6,9 @@ npm publish packages/com.github.asus4.onnxruntime.unity/*.tgz --tag latest
 npm publish packages/com.github.asus4.onnxruntime-extensions/*.tgz --tag latest
 npm publish packages/com.github.asus4.onnxruntime-genai/*.tgz --tag latest
 
-# Disabled due to file size limit in npm
-# npm publish packages/com.github.asus4.onnxruntime.linux-x64-gpu/*.tgz --tag latest
-# npm publish packages/com.github.asus4.onnxruntime.win-x64-gpu/*.tgz --tag latest
+# Large packages: 403 error might occur due to the package size. Comment them out if failed to publish.
+npm publish packages/com.github.asus4.onnxruntime.linux-x64-gpu/*.tgz --tag latest
+npm publish packages/com.github.asus4.onnxruntime.win-x64-gpu/*.tgz --tag latest
 
 echo "Done."
 exit 0
